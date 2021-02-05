ea73e51ac968c32cc4e0678a5a2cfc31ba639e7e
baboomerang <irodrigue101@gmail.com>
2021-02-04 22:46:27 -0500

    polished menubar and added keyboard shortcuts

--- menubar.cpp
+++ menubar.cpp
@@ -7,13 +7,25 @@ Menubar::Menubar(QWidget* parent) : QMainWindow(parent) {
     //TODO: Add status tooltip (when you hover the mouse over the menu option)
     //Change the font of the menubar so it looks nicer
 
-    QMenu* file = menuBar()->addMenu("&File");
-    auto* newfile = new QAction("&New");
-    auto* open = new QAction("&Open");
+    QMenu* filemenu = menuBar()->addMenu("&File");
+    auto* newfile = new QAction("&New", this);
+    auto* open = new QAction("&Open", this);
+    auto* save = new QAction("&Save", this);
+    auto* saveas = new QAction("&Save as", this);
     auto* quit = new QAction("&Exit", this);
-    file->addAction(newfile);
-    file->addAction(open);
-    file->addAction(quit);
+
+    newfile->setShortcut(tr("CTRL+N"));
+    open->setShortcut(tr("CTRL+O"));
+    save->setShortcut(tr("CTRL+S")); 
+    saveas->setShortcut(tr("SHIFT+CTRL+S"));
+    quit->setShortcut(tr("CTRL+Q"));
+
+    filemenu->addAction(newfile);
+    filemenu->addAction(open);
+    filemenu->addAction(save);
+    filemenu->addAction(saveas);
+    filemenu->addSeparator();
+    filemenu->addAction(quit);
 
     QMenu* edit = menuBar()->addMenu("&Edit");
     QMenu* view = menuBar()->addMenu("&View");
