266f6626a65b191d77cfff0b90d68334e79e7c2a
baboomerang <irodrigue101@gmail.com>
2021-02-06 15:14:02 -0500

    updated main driver file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <QApplication>
-#include "menubar.hpp"
+#include "MainWindow.hpp"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    Menubar window;
+    MainWindow window;
     window.resize(800, 600);
     window.setWindowTitle("QToolBar");
     window.show();
