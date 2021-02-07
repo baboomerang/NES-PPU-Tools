fe6fb29dc4f0e1088415d552b146b94a0ca98eba
baboomerang <irodrigue101@gmail.com>
2021-02-06 22:22:14 -0500

    refactored menubar initialization

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,6 +1,7 @@
 #include <QAction>
 #include <QDebug>
 #include <QMenuBar>
+#include <QStatusBar>
 #include <QToolBar>
 
 #include "MainWindow.hpp"
@@ -8,34 +9,58 @@
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     QMenuBar* menubar = menuBar();
+    createActions(menubar);
 
+    QStatusBar* statusbar = statusBar();
+}
+
+MainWindow::~MainWindow() {
+
+}
+
+void MainWindow::createActions(QMenuBar* menubar) {
+
+    //create menus
     QMenu* filemenu = menubar->addMenu(tr("&File"));
-    QAction* newf = new QAction("&New", this);
-    QAction* open = new QAction("&Open", this);
-    QAction* save = new QAction("&Save", this);
-    QAction* saveas = new QAction("&Save as", this);
-    QAction* quit = new QAction("&Exit", this);
+    QMenu* editmenu = menubar->addMenu(tr("&Edit"));
+    QMenu* viewmenu = menubar->addMenu(tr("&View"));
+    QMenu* helpmenu = menubar->addMenu(tr("&Help"));
 
-    filemenu->addAction(newf);
-    filemenu->addAction(open);
-    filemenu->addAction(save);
-    filemenu->addAction(saveas);
-    filemenu->addSeparator();
-    filemenu->addAction(quit);
+    //create dropdown options / "actions"
+    QAction* newf = new QAction("&New...", this);
+    QAction* open = new QAction("&Open...", this);
+    QAction* save = new QAction("&Save...", this);
+    QAction* saveas = new QAction("&Save as...", this);
+    QAction* quit = new QAction("&Quit", this);
+    QAction* about = new QAction("&About", this);
+    QAction* aboutqt = new QAction("&About Qt", this);
 
+    //add keyboard shortcuts and bind them automatically to the choice
     newf->setShortcut(tr("CTRL+N"));
     open->setShortcut(tr("CTRL+O"));
     save->setShortcut(tr("CTRL+S")); 
     saveas->setShortcut(tr("SHIFT+CTRL+S"));
     quit->setShortcut(tr("CTRL+Q"));
 
-    QMenu* editmenu = menubar->addMenu(tr("&Edit"));
-    QMenu* viewmenu = menubar->addMenu(tr("&View"));
-    QMenu* helpmenu = menubar->addMenu(tr("&Help"));
+    //show extra information when user hovers over option
+    newf->setStatusTip(tr("Create a new Nametable"));
+    open->setStatusTip(tr("Open a Nametable"));
+    save->setStatusTip(tr("Save this Nametable"));
+    saveas->setStatusTip(tr("Save this Nametable with a different filename"));
+    quit->setStatusTip(tr("Quit the NESPPU-Tool Suite"));
+    about->setStatusTip(tr("About NESPPU-Tools"));
+    aboutqt->setStatusTip(tr("About Qt"));
+
+    //link dropdown option to the appropriate menu
+    filemenu->addAction(newf);
+    filemenu->addAction(open);
+    filemenu->addAction(save);
+    filemenu->addAction(saveas);
+    filemenu->addSeparator();
+    filemenu->addAction(quit);
+    helpmenu->addAction(about);
+    helpmenu->addAction(aboutqt);
 
     connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
 
-MainWindow::~MainWindow() {
-
-}
