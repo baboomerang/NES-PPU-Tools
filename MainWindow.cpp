59f009ac199d20dc99fe9f1fa0779eb4e9c2e06c
baboomerang <irodrigue101@gmail.com>
2021-02-09 18:27:53 -0500

    use QKeySequence shortcuts instead and finish aboutQt actions

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,6 +1,7 @@
 #include <QAction>
 #include <QFileDialog>
 #include <QMenuBar>
+#include <QMessageBox>
 #include <QStatusBar>
 #include <QToolBar>
 
@@ -23,29 +24,27 @@ MainWindow::~MainWindow() {
 
 }
 
-/* Slots */
+/* SLOTS */
 
 void MainWindow::newfile() {
     return; 
 }
 
 void MainWindow::open() {
-    const QString filename = QFileDialog::getOpenFileName(this);
+    const QString filepath = QFileDialog::getOpenFileName(this);
 
-    if (filename.isEmpty()) {
-        qDebug() << "No filename was given, ignoring open";
+    if (filepath.isEmpty()) {
+        qDebug() << "No filepath was given, ignoring open";
         return;
     }
 
-    qDebug() << "filename was given, filename: " + filename;
-    openFile(filename);
-}
-
-void MainWindow::openFile(const QString& filename) {
-
+    qDebug() << "filepath was given, filepath: " + filepath;
+    openFile(filepath);
 }
 
 void MainWindow::save() {
+    //QString filepath = QFileDialog::getSaveFileName(this, tr("Save A"), filename);
+
     return;
 }
 
@@ -54,7 +53,18 @@ void MainWindow::saveas() {
 }
 
 void MainWindow::about() {
+    QMessageBox::about(this, tr("About NESPPU-TOOLS"),
+            tr("Example description: Insert text here"));
     return;
+}
+
+
+/* FUNCTIONS */
+
+
+/* Opens a file and provides a valid file handler */
+void MainWindow::openFile(const QString& filename) {
+
 }
 
 /* Populate the menubar with menu options and actions */
@@ -75,11 +85,11 @@ void MainWindow::createActions(QMenuBar* menubar) {
     QAction* aboutqt = new QAction("&About Qt", this);
 
     //add keyboard shortcuts and bind them automatically to the choice
-    newf->setShortcut(tr("CTRL+N"));
-    open->setShortcut(tr("CTRL+O"));
-    save->setShortcut(tr("CTRL+S")); 
-    saveas->setShortcut(tr("SHIFT+CTRL+S"));
-    quit->setShortcut(tr("CTRL+Q"));
+    newf->setShortcut(QKeySequence::New);
+    open->setShortcut(QKeySequence::Open);
+    save->setShortcut(QKeySequence::Save);
+    saveas->setShortcut(QKeySequence::SaveAs);
+    quit->setShortcut(QKeySequence::Quit);
 
     //show extra information when user hovers over option
     newf->setStatusTip(tr("Create a new Nametable"));
@@ -101,11 +111,12 @@ void MainWindow::createActions(QMenuBar* menubar) {
     helpmenu->addAction(aboutqt);
 
     //link dropdown option action to the appropriate slot
-    //connect(newf, &QAction::triggered, this, &MainWindow::newfile);
+    connect(newf, &QAction::triggered, this, &MainWindow::newfile);
     connect(open, &QAction::triggered, this, &MainWindow::open);
     connect(save, &QAction::triggered, this, &MainWindow::save);
     connect(saveas, &QAction::triggered, this, &MainWindow::saveas);
     connect(about, &QAction::triggered, this, &MainWindow::about);
+    connect(aboutqt, &QAction::triggered, qApp, QApplication::aboutQt);
     connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
 
